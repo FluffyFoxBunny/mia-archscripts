16b03dc48ca6707937a5703f0e79c712adfe657d
xenolithcluster <46110538+xenolithcluster@users.noreply.github.com>
2021-02-02 17:57:56 +0000

    initial

---
+++ archinstall.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#FUCK IM LAZY THIS DOES IT FOR ME
+
+echo "
+Listen bitch, read the file first,
+don't FUCK THIS UP
+
+make sure you have the partable done, do that
+thing with the numbers and shit. 
+
+type 'jizz' to confirm you're not drunk or high
+
+or dont idgaf
+"
+
+
+read -r jizz
+
+if [ "$jizz" != "jizz" ]; then
+    echo "YOU DUMBASS"
+    exit 1
+fi
+
+echo "YOUVE BEEN WARNED"
+
+
+#============================================================
+
+echo "first my main disk"
+if ! mkfs.ext4 /dev/nvme0n1p4 ; then echo "mkfs.ext4 is fucked" && exit 1 ; fi
+
+echo "swap shit idk why."
+if ! mkswap /dev/sdb3 ; then echo "mkswap is fucked" && exit 1 ; fi
+if ! swapon /dev/sdb3 ; then echo "swapons fucked" && exit 1 ; fi
+
+echo "i guess maybe if that worked we're ok?"
+
+
+
+#====================================================
+
+echo "
+We're mounting now
+"
+
+
+
+if ! mount /dev/nvme0n1p4 /mnt ; then echo "mounting nvme0n1p4 is fucked" && exit 1 ; fi
+
+echo "We're mounted. let's download"
+if ! pacstrap /mnt base linux linux-firmware ; then echo "install is fucked" && exit 1 ; fi
+
+echo "Assuming the arch install didn't shit the bed, we can now gen our fstab"
+
+
+if ! genfstab -U /mnt >> /mnt/etc/fstab ; then echo "genfstab is fucked" && exit 1 ; fi
+
+#============================================================
+
+cd /mnt/etc/ || exit
+
+
+if ! wget https://violetvulpine.net/fuck/archinstall2.sh ; then echo "wget is fucked" && exit 1 ; fi
+
+echo "bitch it's kinda nearly there! 
+do arch-chroot /mnt then start part two, i put the file in /etc/"
---
+++ archinstall2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cd / || exit 1
+
+if ! ln -sf /usr/share/zoneinfo/Europe/Dublin /etc/localtime ; then echo "timezone file fucked" && exit 1 ; fi
+
+hwclock --systohc
+
+locale-gen locale-gen en_US en_US.UTF-8 
+
+touch /etc/hostname
+echo foxxo >> /etc/hostname
+touch /etc/hosts
+echo "
+
+127.0.0.1  localhost
+::1  localhost
+127.0.1.1  foxxo.localdomain  myhostname
+
+" >> /etc/hosts
+
+
+echo "updating mkinitcpio"
+mkinitcpio -P
+
+echo "Time to set root pass."
+
+passwd
+
+pacman -Syu amd-ucode efibootmgr mtools dosfstools vim nvidia cowsay figlet toilet git grub os-prober networkmanager sudo
+
+useradd --create-home mia
+
+
+usermod --append --groups wheel,audio,video,optical,storage mia
+passwd mia
+
+visudo
+
+
+
+echo "grub it up!"
+mkdir /boot/efi
+if ! mount /dev/nvme0n1p1 /mnt/boot/efi ; then echo "mounting efi is fucked" && exit 1 ; fi
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB_UEFI --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+vim /etc/vconsole.conf
+
+echo "all done. you SHOULD be able to reboot now."
+
