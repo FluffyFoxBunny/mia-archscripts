a46cd5831e49a42c5a670ce8329961c884de2da8
VioletVulpine <46110538+VioletVulpine@users.noreply.github.com>
2021-03-05 18:31:44 +0000

    Update archinstall2.sh

--- archinstall2.sh
+++ archinstall2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 EFF=/dev/nvme0n1p1
+MS=/dev/nvme0n1p3
 
 cd / || exit 1
 
@@ -50,10 +51,12 @@ visudo
 
 echo "grub it up!"
 mkdir -p /boot/efi
+mkdir -p /media/mia/win/
+mount $MS /media/mia/win/
 mount $EFF /boot/efi/
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB_UEFI --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
 os-prober
+grub-mkconfig -o /boot/grub/grub.cfg
 
 
 echo "FONT=ter-p24n
